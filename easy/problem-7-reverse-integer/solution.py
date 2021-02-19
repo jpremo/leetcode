5393b910e86efe53e30bdb49245f63913d58f1fa
Jacob <premojacob@gmail.com>
2021-02-18 19:28:47 -0500

    Solved reverse string in python

---
+++ easy/problem-7-reverse-integer/solution.py
@@ -0,0 +1,18 @@
+#https://leetcode.com/problems/reverse-integer/
+import math
+
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+            return False
+        baseStr = str(x)
+        strLength = len(baseStr)
+        maxI = int(math.floor((strLength)/2))
+        for i in range(0, maxI):
+            if baseStr[i] != baseStr[-1-i]:
+                return False
+        return True
