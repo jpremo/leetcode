ce547974eb745c4f84c9f3042335310f2845c9fb
Jacob <premojacob@gmail.com>
2021-02-18 19:13:15 -0500

    Refactored palindrome-number

--- easy/problem-9-palindrome-number.js
+++ easy/problem-9-palindrome-number.js
@@ -1,10 +1,23 @@
 //https://leetcode.com/problems/palindrome-number/
+// const isPalindrome = function(x) {
+//     if(x < 0) return false;
+//     const str = String(x)
+//     let revStr = ''
+//     for(let i = str.length - 1; i >= 0; i--) {
+//         revStr += str[i];
+//     }
+//     return revStr === str;
+// };
+
 const isPalindrome = function(x) {
     if(x < 0) return false;
     const str = String(x)
-    let revStr = ''
-    for(let i = str.length - 1; i >= 0; i--) {
-        revStr += str[i];
+    const len = str.length
+    for(let i = 0; i < len/2; i++) {
+        const iFlip = len-i-1
+        if(str[i] !== str[iFlip]) {
+            return false
+        }
     }
-    return revStr === str;
+    return true
 };
