dc378ac7ba35fffe5b4b3a0dcc756f454d66c3ac
Jacob <premojacob@gmail.com>
2021-03-10 09:38:32 -0500

    Solved problem using memoization

--- medium/problem-139-word-break/solution.js
+++ medium/problem-139-word-break/solution.js
@@ -16,17 +16,39 @@
 //     return false
 // };
 
-var wordBreak = function (s, wordDict) {
-    let table = new Array(s.length + 1)
-    table[0] = true
-    for (let i = 0; i < table.length; i++) {
-        if(table[i]) {
-            for(let j = i + 1; j < table.length; j++) {
-                if(wordDict.includes(s.slice(i,j))){
-                    table[j] = true
-                }
+//tabulation
+
+// var wordBreak = function (s, wordDict) {
+//     let table = new Array(s.length + 1)
+//     table[0] = true
+//     for (let i = 0; i < table.length; i++) {
+//         if(table[i]) {
+//             for(let j = i + 1; j < table.length; j++) {
+//                 if(wordDict.includes(s.slice(i,j))){
+//                     table[j] = true
+//                 }
+//             }
+//         }
+//     }
+//     return table[s.length] === true
+// };
+
+//memoization
+
+var wordBreak = function (s, wordDict, memo = {}) {
+    if(memo[s] !== undefined) return memo[s]
+    if(wordDict.includes(s)) return true
+    if(s.length === 0) return false
+    for ( let i = 0; i < wordDict.length; i ++) {
+        let searchWord = wordDict[i]
+        if (s.startsWith(searchWord)) {
+            const remainder = s.slice(searchWord.length)
+            if (wordBreak(remainder, wordDict, memo)) {
+                return true
+            } else {
+                memo[remainder] = false
             }
         }
     }
-    return table[s.length] === true
+    return false
 };
