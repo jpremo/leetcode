56e8391180b6fce6ae9b0801640ce50d5ef72054
Jacob <premojacob@gmail.com>
2021-03-10 09:19:08 -0500

    Solved problem iteratively

--- medium/problem-139-word-break/solution.js
+++ medium/problem-139-word-break/solution.js
@@ -1,5 +1,32 @@
 //https://leetcode.com/problems/word-break/
 
-var wordBreak = function (s, wordDict) {
+// var wordBreak = function (s, wordDict) {
+//     let stack = [s]
+//     while (stack.length) {
+//         let word = stack.pop()
+//         for (let i = word.length - 1; i >= 0; i--) {
+//             let endFragment = word.slice(i)
+//             let startFragment = word.slice(0, i)
+//             if (wordDict.includes(endFragment)) {
+//                 if(endFragment === word) return true
+//                 stack.push(startFragment)
+//             }
+//         }
+//     }
+//     return false
+// };
 
+var wordBreak = function (s, wordDict) {
+    let table = new Array(s.length + 1)
+    table[0] = true
+    for (let i = 0; i < table.length; i++) {
+        if(table[i]) {
+            for(let j = i + 1; j < table.length; j++) {
+                if(wordDict.includes(s.slice(i,j))){
+                    table[j] = true
+                }
+            }
+        }
+    }
+    return table[s.length] === true
 };
