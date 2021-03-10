b4641387524ca0366e0341fc6a96a025cf3d9771
Jacob <premojacob@gmail.com>
2021-03-10 09:56:03 -0500

    Added memoization solution for 70

--- easy/problem-70-climbing-stairs/solution.js
+++ easy/problem-70-climbing-stairs/solution.js
@@ -1,10 +1,19 @@
 //https://leetcode.com/problems/climbing-stairs/
-var climbStairs = function (n) {
-    let table = new Array(n+1).fill(0)
-    table[0] = 1;
-    table[1] = 1;
-    for(let i = 2; i < table.length; i ++) {
-        table[i] = table[i-1] + table[i-2]
-    }
-    return table[n]
+// var climbStairs = function (n) {
+//     let table = new Array(n+1).fill(0)
+//     table[0] = 1;
+//     table[1] = 1;
+//     for(let i = 2; i < table.length; i ++) {
+//         table[i] = table[i-1] + table[i-2]
+//     }
+//     return table[n]
+// };
+
+var climbStairs = function (n, memo = {}) {
+    if(memo[n] !== undefined) return memo[n]
+    if(n === 2) return 2;
+    if(n === 1) return 1;
+
+    memo[n] = climbStairs(n-1, memo) + climbStairs(n-2, memo)
+    return memo[n]
 };
