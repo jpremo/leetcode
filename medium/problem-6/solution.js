628e9a632c4cd2b8803db660d14eacd7752ea1d2
Jacob <premojacob@gmail.com>
2021-03-15 14:01:33 -0400

    Started solution

--- medium/problem-6/solution.js
+++ medium/problem-6/solution.js
@@ -1,4 +1,27 @@
 //https://leetcode.com/problems/longest-palindromic-substring/
-var longestPalindrome = function (s) {
-
+var longestRightPalindrome = function (s) {
+    let rightIdx = s.length - 1;
+    let cutEnd = s.length;
+    while( rightIdx > 0 ) {
+        leftChar = s[0];
+        rightChar = s[rightIdx]
+        if(leftChar !== rightChar) {
+            cutEnd = rightIdx - 1
+        }
+        rightIdx --
+    }
+    return s.slice(0, cutEnd)
 };
+
+var longestPalindrome = function (s) {
+    let maxPalindrome = s[0]
+
+    for(let i = 0; i <= s.length; i++) {
+        let newMaxPalindrome = longestRightPalindrome(s.slice(0, s.length))
+        if(newMaxPalindrome.length > maxPalindrome.length) {
+            maxPalindrome = newMaxPalindrome
+        }
+    }
+
+    return maxPalindrome
+}
