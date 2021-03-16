4da47d4201c3f59a0f74cf16e2195e4600e8a39b
Jacob <premojacob@gmail.com>
2021-03-16 17:23:23 -0400

    Solved problem 13

--- easy/problem-13/solution.js
+++ easy/problem-13/solution.js
@@ -1,4 +1,33 @@
 //https://leetcode.com/problems/roman-to-integer/
 var romanToInt = function (s) {
+    const translator = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000,
+    }
+    let total = 0;
+    for(let i = 0; i < s.length; i++) {
+        char = s[i]
+        nextChar = s[i+1]
+        subtraction = false;
+        if(char === 'I' && (nextChar === 'V' || nextChar === 'X')) {
+            subtraction = true;
+        } else if (char === 'X' && (nextChar === 'L' || nextChar === 'C')) {
+            subtraction = true;
+        } else if (char === 'C' && (nextChar === 'D' || nextChar === 'M')) {
+            subtraction = true;
+        }
 
+        if(subtraction) {
+            total -= (translator[char]);
+            continue;
+        }
+
+        total += translator[char]
+    }
+    return total
 };
