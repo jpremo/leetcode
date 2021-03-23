017c23c291ec7024969d00ebc9ae4e142803a9a5
Jacob <premojacob@gmail.com>
2021-03-23 16:16:14 -0400

    Solved problem

--- easy/longest-common-prefix/solution.js
+++ easy/longest-common-prefix/solution.js
@@ -7,15 +7,15 @@ var longestCommonPrefix = function (strs) {
         let segment
         for(let i = 0; i < strs.length; i++) {
             let newSegment = strs[i].slice(0, sliceIdx)
-            if(segment === strs[i]) {
+            if(sliceIdx > strs[i].length) {
                 return strs[i]
             }
             if(segment !== undefined && segment !== newSegment) {
                 if(sliceIdx === 1) return ''
-                return strs[i].slice(0,sliceIdx)
+                return strs[i].slice(0,sliceIdx-1)
             }
+            segment = newSegment
         }
-        segment = newSegment
         sliceIdx ++
     }
 };
