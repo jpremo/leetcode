11c08973dbddf5ee7992ec7589a2963a581ff7f3
Jacob <premojacob@gmail.com>
2021-03-23 15:50:31 -0400

    Solved string to integer problem

--- medium/string-to-integer/solution.js
+++ medium/string-to-integer/solution.js
@@ -1,4 +1,38 @@
 //https://leetcode.com/problems/string-to-integer-atoi/
 var myAtoi = function (s) {
-
+    let sign = 1;
+    let firstCharacterPassed = false;
+    let answerStr = ''
+    const digits = '0123456789'
+    for(let i = 0; i < s.length; i ++) {
+        let char = s[i]
+        if(firstCharacterPassed === false) {
+            if(char === ' ') {
+                continue
+            } else {
+                firstCharacterPassed = true
+                if(char === '+') {
+                    sign = 1;
+                } else if (char === '-') {
+                    sign = -1;
+                } else {
+                    if(digits.includes(char)) {
+                        answerStr = answerStr + char
+                    } else {
+                        break;
+                    }
+                }
+            }
+        } else {
+            if (digits.includes(char)) {
+                answerStr = answerStr + char
+            } else {
+                break;
+            }
+        }
+    }
+    let answerNumber = answerStr === '' ? 0 : Number(answerStr)*sign;
+    answerNumber = answerNumber > 2**31 - 1 ? 2**31 - 1 : answerNumber;
+    answerNumber = answerNumber < (-2)**31 ? (-2)**31 : answerNumber;
+    return answerNumber
 };
