1f66755b849be53797aefc769691a38f4105b775
Jacob <premojacob@gmail.com>
2021-03-24 17:02:48 -0400

    solved problem

--- easy/remove-duplicates-from-sorted-array/solution.js
+++ easy/remove-duplicates-from-sorted-array/solution.js
@@ -1,4 +1,14 @@
 //https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 var removeDuplicates = function (nums) {
-
+    for(let i = 0; i < nums.length; i++) {
+        let num = nums[i]
+        let spliceWidth = 0;
+        let j = i + 1;
+        while(nums[j] === num) {
+            spliceWidth++
+            j++
+        }
+        nums.splice(i+1, spliceWidth)
+    }
+    return nums.length
 };
