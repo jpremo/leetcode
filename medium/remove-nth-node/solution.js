d40382cf48225e8a47a1e77e1a8ef8f2e5efe967
Jacob <premojacob@gmail.com>
2021-04-08 08:58:55 -0400

    Added removeNthFromEnd

---
+++ medium/remove-nth-node/solution.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function (head, n) {
+    let window = [null, head, head.next]
+    let counter = 0;
+    let currentNode = head
+    while(currentNode) {
+        currentNode = currentNode.next
+        counter++
+        if(counter >= n) {
+            window = [window[1], window[2], window[2].next]
+        }
+    }
+
+    if(window[0]) {
+        window[0].next = window[2]
+    }
+
+    return (window[1] === head) ? window[2] : head
+};
